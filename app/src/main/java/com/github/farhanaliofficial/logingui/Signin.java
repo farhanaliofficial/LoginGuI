9519016d51b1cbc9c783701c860c094d467c23f4
farhanaliofficial <officialofun@gmail.com>
2024-04-05 13:09:38 +0500

    uploaded files through termux

---
+++ app/src/main/java/com/github/farhanaliofficial/logingui/Signin.java
@@ -0,0 +1,15 @@
+package com.github.farhanaliofficial.logingui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class Signin extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+		
+		}
+}
